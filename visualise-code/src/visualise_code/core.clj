07fea66a8b8ad1798cbf4f7fdef2ec5a14c8860d
Franziska Schmidt <hello@franka.tech>
2017-02-18 16:20:09 +0100

    use current-ns

--- visualise-code/src/visualise_code/core.clj
+++ visualise-code/src/visualise_code/core.clj
@@ -2,6 +2,8 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(def current-ns *ns*)
+
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :hsb)
@@ -14,15 +16,16 @@
 (defn mangoes [] 0)
 (defn mangoses [] 0)
 (defn mangosses [] 0)
-(defn mangoedds [] 0)
+
 
 (defn draw-state [state]
-  (let [fn-count (count (keys (ns-publics 'visualise-code.core)))
+  (let [fn-count (count (keys (ns-publics current-ns)))
         color (* (/ 1 (- fn-count 3) ) 255)]
     (q/background color)
     (q/stroke 0)
     (q/text (str fn-count) 20 20)
-    (q/text (str color) 20 40)))
+    (q/text (str color) 20 40)
+    (q/text (str current-ns) 20 60)))
 
 (q/defsketch visualise-code
   :title "visualise the code"
