af33174306515cce11764674d80d6de4b4ae5e9a
Franziska Schmidt <hello@franka.tech>
2017-02-21 21:35:48 +0100

    updates to quil sketch

--- visualise-code/src/visualise_code/core.clj
+++ visualise-code/src/visualise_code/core.clj
@@ -10,22 +10,34 @@
   {:color 0
    :angle 0})
 
-(defn update-state [state]
-  state)
+(def circle-colour 90)
 
-(defn mangoes [] 0)
-(defn mangoses [] 0)
-(defn mangosses [] 0)
+(defn draw-circle [n init-x init-y]
+  (q/fill (+ circle-colour (* n 5)))
+  (q/ellipse (+ init-x (* n 50)) (+ init-y (* n 8)) 30 30))
 
+(defn draw-many-circles []
+  (doseq [n (range 10)
+          y [160 200 240 280 320 360 400 440 480]]
+    (draw-circle n 20 y)))
 
-(defn draw-state [state]
+(defn draw-status-bar [state]
   (let [fn-count (count (keys (ns-publics current-ns)))
-        color (* (/ 1 (- fn-count 3) ) 255)]
+       color (* (/ 1 (- fn-count 3) ) 255)]
     (q/background color)
-    (q/stroke 0)
+    (q/fill 255)
+    (q/rect 0 0 (q/width) 100)
+    (q/fill 0)
     (q/text (str fn-count) 20 20)
     (q/text (str color) 20 40)
     (q/text (str current-ns) 20 60)))
+
+(defn draw-state [state]
+  (draw-status-bar state)
+  (draw-many-circles))
+
+(defn update-state [state]
+  state)
 
 (q/defsketch visualise-code
   :title "visualise the code"
