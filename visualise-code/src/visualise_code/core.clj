35d00392cc488df5600860402b0caa696daa670c
Franziska Schmidt <hello@franka.tech>
2017-07-07 16:59:04 +0200

    visualise updates, new images

--- visualise-code/src/visualise_code/core.clj
+++ visualise-code/src/visualise_code/core.clj
@@ -19,35 +19,26 @@
 ;(defn draw-many-rects []
 ;  (doseq [x (range 10)] (draw-rect 0 x)))
 
-(defn draw-circle [colour n init-x init-y]
-  (q/no-stroke)
-  (q/fill (mod (+ colour (/ init-y 4)) 255))
-  (q/ellipse (+ init-x (* n 50)) (+ init-y (* n 8)) 30 30))
-
-(defn draw-many-circles []
-  (doseq [n (range (mod (q/frame-count) 12))
-          y [120 160 200 240 280 320 360 400 440 480]]
-    (draw-circle n n 20 y)))
-
 (defn draw-background [state]
   (let [fn-count (:fn-count state)
-        h (mod (* 20 fn-count) 360)
-        s 130
+        h (mod (* 18 fn-count) 360)
+        s 160
         b 200]
     (q/background (q/color h s b))))
 
 (defn draw-fn-circles [state]
   (doseq [n (range (:fn-count state))]
-    (q/fill (rand-int 255))
-    (q/ellipse 20 (+ (* n 25) 90) 20 20)))
+    (q/fill (- 230 (* 15 n)))
+    (q/ellipse 50 (+ (* n 25) 50) 20 20)))
 
-(defn draw-status-bar [state]
+(defn draw-stats [state]
   (let [fn-count (:fn-count state)
        color (* (/ 1 (- fn-count 3)) 255)]
     (q/fill 0)
-    (q/text (str "Function count: " fn-count) 250 250)
-    (q/text (str "Current ns: " current-ns) 250 200)
-    (q/text (str "Frame count: " (q/frame-count)) 350 50)))
+    (q/text-size 30)
+    (q/text-align :right)
+    (q/text (str "Function count: " fn-count) 450 250)
+    (q/text (str "ns: " current-ns) 450 200)))
 
 (defn clear-canvas []
   (q/background 10))
@@ -56,10 +47,9 @@
   (= (mod (q/frame-count) n) 0))
 
 (defn draw-state [state]
-  (when (every-n-frames 5)
-    (draw-background state)
-    (draw-fn-circles state))
-  (draw-status-bar state))
+  (draw-background state)
+  (draw-fn-circles state)
+  (draw-stats state))
 
 (defn update-state [state]
   (assoc state :fn-count (count (keys (ns-publics current-ns)))))
